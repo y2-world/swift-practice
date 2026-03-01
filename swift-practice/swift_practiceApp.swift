ead613570ca98af444eedf4bbc01b897c7f0c961
Yuki <yuki92496@gmail.com>
2026-03-01 22:05:48 +0900

    Initial Commit

---
+++ swift-practice/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  swift-practice
+//
+//  Created by Yuki Yoshida on 2026/03/01.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
